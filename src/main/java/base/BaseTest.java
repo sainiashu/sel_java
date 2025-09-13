64b15efea5aabacb0e6b505de7eb80c055df802f
sainiashu <saini.ashu90@gmail.com>
2025-09-13 07:41:50 -0400

    implemented screenshot method

--- src/main/java/base/BaseTest.java
+++ src/main/java/base/BaseTest.java
@@ -1,5 +1,6 @@
 package base;
 
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.openqa.selenium.WebDriver;
@@ -9,6 +10,7 @@ import org.testng.annotations.BeforeSuite;
 
 import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.MediaEntityBuilder;
 
 import utils.ExtentReportManager;
 import utils.Log;
@@ -44,12 +46,21 @@ public class BaseTest {
 	
 	
 	@AfterMethod
-	public void tearDown() {
-		if(driver!=null) {
-			Log.info("quit the browser");
-			driver.quit();
+	public void tearDown(ITestResult result) {
+		
+		if(result.getStatus() == ITestResult.FAILURE) {
+			
+			String screenshotPath = ExtentReportManager.captureScreenshot(driver, "LoginFailure");
+			test.fail("Test Failed.. Check Screenshot", 
+					MediaEntityBuilder.createScreenCaptureFromPath(screenshotPath).build());
 		}
 		
+		
+		if (driver != null) {
+			Log.info("Closing Browser...");
+			driver.quit();
+		}
 	}
+
 
 }
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 //import org.junit.jupiter.api.Test;
@@ -10,23 +11,45 @@ import utils.ExtentReportManager;
 import utils.Log;
 
 public class LoginTest extends BaseTest {
-	
+
 	@Test
 	public void testValidLogin() {
-		
+
 		Log.info("Start Test...");
 		test = ExtentReportManager.createTest("Test 1 test Valid Login ");
-		
+
 		Log.info("Enter Credentials");
 		LoginPage loginPage = new LoginPage(driver);
 		loginPage.enterUserName("admin@yourstore.com");
 		loginPage.enterPassword("admin");
-		
+
 		Log.info("Click Login");
 		loginPage.clickLogin();
-		
+
+		Assert.assertEquals(driver.getTitle(), "Dashboard / nopCommerce administration");
+
 		Log.info("Login Pass");
-	
+
+	}
+
+	@Test
+	public void testInvalidLogin() {
+
+		Log.info("Start Test...");
+		test = ExtentReportManager.createTest("Test 2 test Invalid Login ");
+
+		Log.info("Enter Credentials");
+		LoginPage loginPage = new LoginPage(driver);
+		loginPage.enterUserName("admin@yourstore.com");
+		loginPage.enterPassword("admin");
+
+		Log.info("Click Login");
+		loginPage.clickLogin();
+
+		Assert.assertEquals(driver.getTitle(), "Just a moment...1232222");
+
+		Log.info("Login Pass");
+
 	}
 
 }
